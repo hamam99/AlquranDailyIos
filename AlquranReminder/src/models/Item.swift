1828fdaae95ca5321cd1caa73879b82b005c07a2
Hamam <zero.saturnus@gmail.com>
2026-08-31 16:12:49 +0800

    add sample model db

---
+++ AlquranReminder/src/models/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AlquranReminder
+//
+//  Created by Hamam Nasrodin on 30/08/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
